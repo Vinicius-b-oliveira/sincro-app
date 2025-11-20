6719ebd1f04a3200e217c472fa4afb1186116d65
Vinicius Bueno <viniciusbuenodeoliveira2017@gmail.com>
2025-11-20 17:26:48 -0300

    feat(core): adiciona SplashView em core/widgets para feedback visual de inicialização

---
+++ lib/core/widgets/splash_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:sincro/core/theme/config/app_colors.dart';
+import 'package:sincro/core/widgets/app_logo.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const AppLogo(
+              width: 150,
+              height: 150,
+            ),
+            const SizedBox(height: 32),
+            const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
